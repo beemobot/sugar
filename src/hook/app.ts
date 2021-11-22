bef50c20ffa81def35d5f9c4860ceb1f8fae9a2c
Ayu <ayu@ayu.dev>
2021-11-22 08:26:43 -0800

    Add Helmet & Port in Config

--- src/hook/app.ts
+++ src/hook/app.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import helmet from "helmet";
 import lastUpdate from "../objects/lastUpdate";
+import { HOOK_PORT } from "../config.json";
 
 const app = express();
 const port = 3030;
 
+app.use(helmet());
 app.post("/hook", (req, res, next) => {
     setTimeout(() => {
         const webhookUrl = req.url;
@@ -14,6 +17,6 @@ app.post("/hook", (req, res, next) => {
     res.status(200).send("OK");
 });
 
-app.listen(port, () => {
+app.listen(HOOK_PORT, () => {
     console.log(`Hook listening on http://localhost:${port}`);
 });
