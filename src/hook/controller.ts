a448c81a300ef2dd946ff3c4bacd5b1bc6c1fb0a
Luis Rizo <25105030+LuisRizo@users.noreply.github.com>
2022-02-23 05:20:47 +0000

    Setup prisma and use tea service

--- src/hook/controller.ts
+++ src/hook/controller.ts
@@ -1,33 +1,74 @@
 import { Event } from "chargebee-typescript/lib/resources";
 import { RouterContext } from "@koa/router";
+import {
+  activateSubscription,
+  cancelSubscription,
+  changeSubscription,
+} from "./services/tea";
 
-const allowed_events: Event["event_type"][] = [
-  "subscription_activated",
-  "subscription_started",
-  "subscription_paused",
-  "subscription_cancelled",
-];
+export class HookError extends Error {
+  public status: number;
+  public metadata: Record<string, unknown>;
+
+  public constructor(
+    message: string,
+    status: number = 500,
+    errorMetadata: Record<string, unknown> = {}
+  ) {
+    super(message);
+    this.name = "HookError";
+    this.status = status;
+    this.metadata = errorMetadata;
+  }
+}
+
+const handleEvent = async (event: Event) => {
+  switch (event.event_type) {
+    case "subscription_activated":
+    case "subscription_started":
+    case "subscription_reactivated":
+    case "subscription_renewed":
+    case "subscription_resumed":
+      await activateSubscription();
+      break;
+
+    case "subscription_changed":
+      await changeSubscription();
+      break;
+
+    case "subscription_paused":
+    case "subscription_deleted":
+    case "subscription_cancelled":
+      await cancelSubscription();
+      break;
+    default:
+      throw new HookError("Unsupported event", 404);
+  }
+};
 
 export const hookController = async (ctx: RouterContext) => {
-  const { event_type: eventType } = ctx.request.body as Event;
-  console.log("Received hookController event", {
-    body: ctx.request.body,
-    eventType: eventType,
+  const event = ctx.request.body as Event;
+  const { event_type: eventType, id: eventID } = event;
+
+  console.log("[hookController] Received hookController event", {
+    eventID,
+    eventType,
   });
 
-  if (!allowed_events.includes(eventType || "")) {
-    console.log("process request found an unknown event", {
-      event: eventType,
+  try {
+    await handleEvent(event);
+
+    ctx.status = 200;
+    ctx.body = {};
+  } catch (err) {
+    const error = err as HookError;
+    console.error("[hookController] Error in hook", {
+      eventType,
+      errorMessage: error.message,
+      errorMetadata: error.metadata,
     });
 
-    ctx.status = 404;
-    ctx.body = {};
-    return;
+    ctx.status = error.status;
+    ctx.body = { errorMessage: error.message };
   }
-
-  // TODO: Handle events
-  console.log("Valid event: " + eventType);
-
-  ctx.status = 200;
-  ctx.body = {};
 };
---
+++ src/hook/services/tea.ts
@@ -0,0 +1,9 @@
+export const activateSubscription = async () => {
+  // TODO: @LuisRizo build this
+};
+export const changeSubscription = async () => {
+  // TODO: @LuisRizo build this
+};
+export const cancelSubscription = async () => {
+  // TODO: @LuisRizo build this
+};
